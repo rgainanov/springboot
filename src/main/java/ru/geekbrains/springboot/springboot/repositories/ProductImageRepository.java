9f87d5c3e9f7a691dd1fc1960b8aa06068152ae9
Romans Gainanovs <gainanov@amazon.com>
2022-01-16 17:32:07 +0000

    1

--- src/main/java/ru/geekbrains/springboot/springboot/repositories/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.springboot.springboot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.springboot.springboot.models.ProductImage;
-
-@Repository
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-}
